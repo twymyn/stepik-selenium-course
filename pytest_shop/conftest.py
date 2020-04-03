86596073636fdff8785055579444420de4569696
Tina <lc.tinatran@gmail.com>
2020-04-03 14:21:07 +0200

    Reformatted to PEP 8

--- pytest_shop/conftest.py
+++ pytest_shop/conftest.py
@@ -2,18 +2,20 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
+
 def pytest_addoption(parser):
-    parser.addoption('--browser_name', action='store', default="chrome", 
-        help="Choose browser: chrome or firefox")
-    parser.addoption('--language', action='store', default="en", 
-        help="Choose language as 2 letters code. Example: en, es, ru")
+    parser.addoption('--browser_name', action='store', default="chrome",
+                     help="Choose browser: chrome or firefox")
+    parser.addoption('--language', action='store', default="en",
+                     help="Choose language as 2 letters code. Example: en, es, ru")
+
 
 @pytest.fixture(scope="function")
 def browser(request):
     browser_name = request.config.getoption("browser_name")
     language = request.config.getoption("language")
     browser = None
-    
+
     if browser_name == "chrome":
         print(f"\nstart Chrome[{language}] browser for test..")
         options = Options()
--- pytest_shop/test_items.py
+++ pytest_shop/test_items.py
@@ -2,6 +2,7 @@ import pytest
 
 urls = ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"]
 
+
 @pytest.mark.parametrize('url', urls)
 def test_page_has_add_to_basket_button(browser, url):
     browser.get(url)
